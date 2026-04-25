5b7a12f3291baccf0a9c73a10da32481f8d6bef8
Dhanush <iamdk353@gmail.com>
2026-04-25 12:39:20 +0530

    inculusion of front end origin

--- app/main.py
+++ app/main.py
@@ -96,7 +96,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+      allow_origins=["https://agrinex-sage.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
